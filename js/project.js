6576f562173cc6c9eddf59aa15121a9d0580a8d5
Markus von Staden <markus@von-staden.net>
2021-08-29 21:31:05 +0200

    read project dynamically from json

--- js/project.js
+++ js/project.js
@@ -2,13 +2,35 @@ let root = document.documentElement;
 let modal = document.querySelector("#modal");
 let modalContent = document.querySelector("#modal-content");
 
+let title = document.querySelector("#title");
+let subtitle = document.querySelector(".subtitle"); 
+let description = document.querySelector("#description");
+let gallery = document.querySelector("#gallery");
+
+const urlparams = new URLSearchParams(window.location.search);
+
+project = fetch("projects/"+urlparams.get("project")+".json")
+    .then(results => results.json())
+    .then(data => {
+        document.title = data.title;
+        title.innerText = data.title;
+        subtitle.innerText = data.languages;
+        description.innerText = data.description;
+        data.images.forEach(img => {
+            let elem = document.createElement("img");
+            elem.src = img;
+            elem.setAttribute("onclick", "enableModal(this)");
+            gallery.appendChild(elem);
+        })
+    });
+
 window.addEventListener("popstate", closeModal);
 
 dark = false;
 
 function enableModal(el) {
     modal.style.display = "flex";
-    modalContent.src = el.getAttribute("src").replace(".jpg", ".pdf");
+    modalContent.src = el.getAttribute("src");
     window.history.pushState(null, null, null);
 }
 
